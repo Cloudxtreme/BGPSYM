be4b7e567146286deba6376b6c4c7d561ff2f5c0
Jeffrey <walkingsoul@gmail.com>
2013-07-08 12:14:20 +0200

    Altered log output
    - Added state of neighbor
    - Added filtered prefixes
    - Added copy of map

--- src/nl/nlnetlabs/bgpsym01/cache/ResultWriterLog.java
+++ src/nl/nlnetlabs/bgpsym01/cache/ResultWriterLog.java
@@ -10,17 +10,23 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Collection;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.Map.Entry;
 
 import nl.nlnetlabs.bgpsym01.main.Tools;
 import nl.nlnetlabs.bgpsym01.command.RouteViewDataResponse;
 import nl.nlnetlabs.bgpsym01.primitives.BGPSymException;
 import nl.nlnetlabs.bgpsym01.primitives.bgp.ASIdentifier;
+import nl.nlnetlabs.bgpsym01.primitives.bgp.Prefix;
 import nl.nlnetlabs.bgpsym01.primitives.factories.XStreamFactory;
 import nl.nlnetlabs.bgpsym01.process.BGPProcess;
 import nl.nlnetlabs.bgpsym01.xstream.XProperties;
 import nl.nlnetlabs.bgpsym01.neighbor.Neighbor;
 import nl.nlnetlabs.bgpsym01.route.PeerRelation;
 import nl.nlnetlabs.bgpsym01.route.PrefixStoreMapImpl;
+import nl.nlnetlabs.bgpsym01.route.output.OutputBufferImpl;
+import nl.nlnetlabs.bgpsym01.route.output.OutputStateImpl;
 
 import org.apache.log4j.Logger;
 import com.thoughtworks.xstream.XStream;
@@ -41,11 +47,14 @@ public class ResultWriterLog {
 
 	private List<Collection<RouteViewDataResponse>> responseList;
 	
+	private List<Map<Neighbor, List<Prefix>>> filtered;
+	
 	public ResultWriterLog(ASIdentifier asId) {
 		this.asId = asId;
 		this.logs = new ArrayList<String>();
 		xStream = XStreamFactory.getXStream();
 		responseList = new ArrayList<Collection<RouteViewDataResponse>>();
+		filtered = new ArrayList<Map<Neighbor, List<Prefix>>>();
 	}
 
     OutputStream getStream() {
@@ -64,6 +73,29 @@ public class ResultWriterLog {
 	String getFilename(ASIdentifier asId) {
 		return getDirectory().getAbsolutePath() + File.separator + OUTPUT_FILENAME_PREFIX + asId.toString();
 	}
+	
+	public Map<Neighbor, List<Prefix>> clone (Map<Neighbor, List<Prefix>> filteredPrefixes) {
+		Iterator<Entry<Neighbor, List<Prefix>>> iterator = filteredPrefixes.entrySet().iterator();
+		
+		Map<Neighbor, List<Prefix>> clone = new TreeMap<Neighbor, List<Prefix>>();
+		
+		while (iterator.hasNext()) {
+			Entry<Neighbor, List<Prefix>> entry = iterator.next();
+			Neighbor key = entry.getKey();
+			List<Prefix> value = entry.getValue();
+			
+			List<Prefix> backup = new ArrayList<Prefix>();
+			
+			for (Prefix pr : value) {
+				backup.add(pr);
+			}
+			
+			clone.put(key, backup);
+			
+		}
+		
+		return clone;
+	}
 
 	public void writeLog (BGPProcess process, long currentTime) {
 			String state = "<l t=\""+currentTime+"\"" +
@@ -76,13 +108,18 @@ public class ResultWriterLog {
 
 			while (neighbors.hasNext()) {
 				Neighbor neighbor = neighbors.next();
-				state += "<n r=\""+(PeerRelation)neighbor.getAttachment()+"\">"+neighbor.getASIdentifier().getId()+"</n>";
+				state += "<n r=\""+(PeerRelation)neighbor.getAttachment()+"\" v=\""+neighbor.isValid()+"\">"+neighbor.getASIdentifier().getId()+"</n>";
 			}
 
 			state += "</ns>";
 			
 			Collection<RouteViewDataResponse> prefixDataList = store.getPrefixDataList();
 			responseList.add(prefixDataList);
+			
+			OutputBufferImpl outputBuffer = (OutputBufferImpl) store.getOutputBuffer();
+			OutputStateImpl outputState = (OutputStateImpl) outputBuffer.getOutputState();
+			
+			filtered.add(clone(outputState.getPrefixes()));
 
 			logs.add(state);
 	}
@@ -98,14 +135,34 @@ public class ResultWriterLog {
 				
 				for (String log : logs) {
 					stream.write(log.getBytes());
-					Iterator<RouteViewDataResponse> iterator = responseList.get(i).iterator();
-
+					
+					
+					stream.write("<f>".getBytes());
+					Iterator<Entry<Neighbor, List<Prefix>>> iteratorPrefixes = this.filtered.get(i).entrySet().iterator();
+					while (iteratorPrefixes.hasNext()) {
+						Entry<Neighbor, List<Prefix>> entry = iteratorPrefixes.next();
+						Neighbor neighbor = entry.getKey();
+						
+						String n = "<n as=\""+neighbor.getASIdentifier()+"\">";
+						stream.write(n.getBytes());
+						
+						for (Prefix prefix : entry.getValue()) {
+							String pr = "<p>\""+prefix+"\"</p>";
+							stream.write(pr.getBytes());
+						}
+						
+						stream.write("</n>".getBytes());
+					}
+					
+						
+					stream.write("</f>".getBytes());
 					stream.write("<rs>".getBytes());
 					
+					Iterator<RouteViewDataResponse> iteratorResponses = responseList.get(i).iterator();
 					RouteViewDataResponse currentResponse;
-					while (iterator.hasNext()) {
+					while (iteratorResponses.hasNext()) {
 							
-						currentResponse = iterator.next();
+						currentResponse = iteratorResponses.next();
 						CompactWriter writer = new CompactWriter(new OutputStreamWriter(stream));
 						xStream.marshal(currentResponse, writer);
 					}
